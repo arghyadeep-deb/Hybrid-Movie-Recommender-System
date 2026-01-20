36759700a79d5934f77905c976a7458023204913
Arghyadeep Deb <arghyadeepdeb020703@gmail.com>
2026-01-20 12:54:56 +0530

    Add correct Drive file IDs for all models

--- download_models.py
+++ download_models.py
@@ -6,9 +6,9 @@ os.makedirs(MODEL_DIR, exist_ok=True)
 
 FILES = {
     "cf_sim.pkl": "1FPWuKFbJ6Y-TOYixPo6VMDoliJg8vPYh",
-    "content_sim.pkl": "PASTE_CONTENT_SIM_FILE_ID",
-    "metadata.pkl": "PASTE_METADATA_FILE_ID",
-    "tfidf.pkl": "PASTE_TFIDF_FILE_ID",
+    "content_sim.pkl": "1wlkznyMPB5nFkenEg2boXW0XEFEe_qA2",
+    "metadata.pkl": "1NE5hk92lOJH5Lqdg7XgT2tWtRBubhwOy",
+    "tfidf.pkl": "1HMl0cEw3_9hXVFkXhO-_lzG0xstY85pG",
 }
 
 def download(file_id, output_path):
