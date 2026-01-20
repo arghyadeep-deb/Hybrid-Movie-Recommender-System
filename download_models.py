aa34ab2d0cb5cce6d19fb4081abb4d483496a812
Arghyadeep Deb <arghyadeepdeb020703@gmail.com>
2026-01-20 11:57:23 +0530

    Install gdown and fix Google Drive downloads

--- download_models.py
+++ download_models.py
@@ -5,10 +5,10 @@ MODEL_DIR = "model"
 os.makedirs(MODEL_DIR, exist_ok=True)
 
 FILES = {
-    "cf_sim.pkl": "https://drive.google.com/uc?id=1FPWuKFbJ6Y-TOYixPo6VMDoliJg8vPYh",
-    "content_sim.pkl": "https://drive.google.com/uc?id=1wlkznyMPB5nFkenEg2boXW0XEFEe_qA2",
-    "metadata.pkl": "https://drive.google.com/uc?id=1NE5hk92lOJH5Lqdg7XgT2tWtRBubhwOy",
-    "tfidf.pkl": "https://drive.google.com/uc?id=1HMl0cEw3_9hXVFkXhO-_lzG0xstY85pG",
+    "cf_sim.pkl": "1FPWuKFbJ6Y-TOYixPo6VMDoliJg8vPYh",
+    "content_sim.pkl": "1wlkznyMPB5nFkenEg2boXW0XEFEe_qA2",
+    "metadata.pkl": "1NE5hk92lOJH5Lqdg7XgT2tWtRBubhwOy",
+    "tfidf.pkl": "1HMl0cEw3_9hXVFkXhO-_lzG0xstY85pG",
 }
 
 def download_file(url, dest):
